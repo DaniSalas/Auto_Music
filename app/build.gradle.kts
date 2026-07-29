0af16aafa9e8739f22c2cae73e478132acc13746
DaniSalas <danielsalas@telefonica.net>
2026-07-29 18:38:11 +0200

    pasem a versió 2.0 i canviem el id a com.danielsalas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,15 +7,15 @@ plugins {
 }
 
 android {
-    namespace = "com.example.auto_music"
+    namespace = "com.danielsalas.auto_music"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.auto_music"
+        applicationId = "com.danielsalas.auto_music"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
